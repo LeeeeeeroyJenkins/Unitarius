fc1531830cc436250e515af51991157985ea7083
LeeeeeeroyJenkins <54649953+LeeeeeeroyJenkins@users.noreply.github.com>
2022-07-13 20:10:05 +0400

    Update 2.sql

--- 2.sql
+++ 2.sql
@@ -1,10 +1,39 @@
-SELECT line.author, birthday, book, Count(*) as quantity
-FROM (
-SELECT Tabb.author, birthday, book FROM Tabb
-LEFT JOIN Taba ON Taba.author = Tabb.author
-UNION
-SELECT Taba.author, birthday, book FROM Taba
-LEFT JOIN Tabb ON Taba.author = Tabb.author
-ORDER BY author) as line
-GROUP BY line.author
-HAVING  Count(*) > 1
+with 
+line as (
+  SELECT 
+    Tabb.author, 
+    birthday, 
+    book 
+  FROM Tabb
+
+  LEFT JOIN Taba 
+  ON Taba.author = Tabb.author
+
+  UNION
+
+  SELECT 
+    Taba.author, 
+    birthday, 
+    book 
+  FROM Taba
+
+  LEFT JOIN Tabb 
+  ON Taba.author = Tabb.author
+
+  ORDER BY author
+),
+  line2 as (
+  SELECT 
+    line.author
+  FROM line
+  GROUP BY line.author
+  HAVING  Count(*) > 1
+)
+
+SELECT 
+  line.author, 
+  line.birthday, 
+  line.book
+FROM line
+right join line2
+ON line2.author = line.author
