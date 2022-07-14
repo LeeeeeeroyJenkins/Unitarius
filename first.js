e3638c100c4974cb07468461d27cfe5507433438
LeeeeeeroyJenkins <54649953+LeeeeeeroyJenkins@users.noreply.github.com>
2022-07-14 22:50:49 +0400

    Update first.js

--- first.js
+++ first.js
@@ -1,21 +1,38 @@
-let task_1 = (n = 10, m = 3) => { 
+let arrayToEnd = (n = 10, m = 3) => {
   // Create array
-  let array1 = Array(n).fill().map((element, i) => (i+1) + '_element')
-  console.log(array1)
+  let array = Array(n)
+    .fill()
+    .map((element, i) => i + 1 + "_element");
+  console.log(array);
 
-  let i = 0, i_new, tmp, tmp_number
-  
+  let i = 0,
+    i_new,
+    tmp,
+    tmp_number,
+    mod = n % m;
+
   while (i < n) {
-    i_new = i + m
-    if (i_new >= n) i_new = n - 1
-    else tmp_number = n - 1 // for balance
+    if (i + m >= n) m = n - i - mod;
+    else tmp_number = n - i - mod; // for balance
 
-    tmp = array1[i];
-    array1[i] = array1[i_new]
-    array1[i_new] = tmp
-    
-    i++
-  } 
-  console.log(array1)
-}
-task_1(10, 3)
+    i_new = i + m;
+
+    if (
+      i <= i_new && // for balance
+      i_new !== n
+    )
+      tmp_number = i; // for balance
+    else i_new = i;
+
+    tmp = array[i];
+    array[i] = array[i_new];
+    array[i_new] = tmp;
+
+    console.log({ i: i, i_new: i_new });
+    console.log(array);
+    i++;
+  }
+  console.log("Answer:");
+  console.log(array);
+};
+arrayToEnd(10, 7);
